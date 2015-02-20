f8698875afe255edda5543aff95300087b23f6d2
Derrick <derrick.b.gold@gmail.com>
2015-02-19 21:54:44 -0700

    Fix text centering in Chrome for Windows.

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function cloneSelection(appendTo,toCopy, className) {
 		d3.select(clone).attr("class", className);
   });
   return appendTo;
-}			
+}
 
 
 
-//create our zoomHandler so we can translate it by our 
+//create our zoomHandler so we can translate it by our
 //own drag position later on
 var ZoomHandler = ( function() {
 	var instance;
@@ -23,7 +23,7 @@ var ZoomHandler = ( function() {
 		timer: 0,
 
 		__zoomStartCb: 0,
-		startZoom: function(v) { 
+		startZoom: function(v) {
 			instance.__zoomStartCb = function(z) {v(instance); };
 		},
 
@@ -46,9 +46,9 @@ var ZoomHandler = ( function() {
 				clearTimeout(instance.timer);
 				instance.timer = setTimeout(instance.__zoomEndCb, 200);
 
-				instance.__zoomDuring();					
+				instance.__zoomDuring();
 		},
-		z: d3.behavior.zoom().on("zoom", function(){instance.zoomBehavior();})	
+		z: d3.behavior.zoom().on("zoom", function(){instance.zoomBehavior();})
 	}}
 
 	return {
@@ -83,7 +83,7 @@ var ChartScaler = ( function() {
 			xRange: [0, 0],
 			xDomain:[0, 100],
 
-			yAxis: d3.svg.axis().orient("left"),	
+			yAxis: d3.svg.axis().orient("left"),
 			yScale: d3.scale.linear(),
 			yTickCount: 10,
 			yTicks: function(e) {
@@ -100,18 +100,18 @@ var ChartScaler = ( function() {
 				instance.xTicks((temp > instance.maxTicks) ? instance.maxTicks : temp);
 
 				temp = instance.yTickCount * s;
-				instance.yTicks((temp > instance.maxTicks) ? instance.maxTicks : temp);	
+				instance.yTicks((temp > instance.maxTicks) ? instance.maxTicks : temp);
 
 
 
-				var newXRange = [instance.xRange[0], instance.xRange[1] * s];	
+				var newXRange = [instance.xRange[0], instance.xRange[1] * s];
 				var newYRange = [instance.yRange[0], instance.yRange[1] * s];
 
 
 				instance.xScale.domain(instance.xDomain).range(newXRange);
 				instance.yScale.domain(instance.yDomain).range(newYRange);
 				instance.xAxis.scale(instance.xScale);
-				instance.yAxis.scale(instance.yScale);	
+				instance.yAxis.scale(instance.yScale);
 			}
 
 
@@ -140,20 +140,20 @@ function createLegend(sectors, filterFn) {
 	var legendContentWidth = 1000;
 	var legendContentHeight = 50;
 	var textSpacing = 10;
-	
+
 	var sideBar = d3.select("#sectorBox").append("svg")
 					.attr("class", "sidebar")
 					.attr("width", window.innerWidth/2).attr("height", "50")
 					.attr("viewBox", "0 0 " + legendContentWidth + " " +
 						  legendContentHeight);
-	
+
 	sideBar.append("rect").attr("width", "100%").attr("height", "100%")
 			.attr("fill", "black")
 			.attr("stroke", "black").append("g");
-					
-	
+
+
 	function sectColor(d) { return d[d3.keys(d)[0]]; }
-	
+
 	var sectorIDNum = 0;
 	var nextPos = textSpacing;
 	var labels = sideBar.selectAll("text").data(sectors).enter()
@@ -174,21 +174,21 @@ function createLegend(sectors, filterFn) {
 					.attr("x", function(d) {
 						var pos = nextPos;
 						nextPos += this.getComputedTextLength() + textSpacing;
-						return pos;	
+						return pos;
 					})
 					.attr("y", "50%")
 					.on("mouseover", function(d) {
-						
+
 						if(d.this.attr("enabled") != "yes")
 							d.this.attr("fill", "white");
 						else
 							d.this.attr("fill", sectColor);
-						
+
 					})
 					.on("mouseout", function(d) {
 						var obj = d.this
-						
-						if(d.this.attr("enabled") == "yes") 
+
+						if(d.this.attr("enabled") == "yes")
 							d.this.attr("fill", "white");
 						else
 							d.this.attr("fill", function(){ return sectColor(d); });
@@ -196,29 +196,32 @@ function createLegend(sectors, filterFn) {
 					.on("click", function(d) {
 						if(d.this.attr("enabled")) {
 							d.this.attr("enabled", null);
-							d.this.style("text-decoration", null);	
+							d.this.style("text-decoration", null);
 						} else {
 							d.this.attr("enabled", "yes");
-							d.this.style("text-decoration", "line-through");	
+							d.this.style("text-decoration", "line-through");
 						}
 						filterFn(d);
 					});
-	
+
 }
 
 
 document.addEventListener("DOMContentLoaded", function(e) {
 	
-	var width = window.innerWidth, 
+
+	
+
+	var width = window.innerWidth,
 		height = window.innerHeight,
 		xPadding = 40, yPadding = 20;
 
 	//configure plot data size
-	var pointWidth = 0.5, pointHeight = 0.5, 
+	var pointWidth = 0.5, pointHeight = 0.5,
 		pointContentWidth = 150, pointContentHeight = 150,
 		cullMin = pointWidth, cullMax = 600,
 		epsScaleFactor = 10;
-	
+
 	var XTitleX = 500,
 		XTitleY = height/2 + 20;
 
@@ -235,13 +238,13 @@ document.addEventListener("DOMContentLoaded", function(e) {
 		{"Utilities": "SpringGreen" }
 	];
 
-	
+
 
 	var zoomHandler = ZoomHandler.init({
 		maxZoom: 100
 	});
 	var chartScaler = ChartScaler.init({
-		xRange: [xPadding,  width - xPadding], 
+		xRange: [xPadding,  width - xPadding],
 		yRange: [yPadding, height - yPadding]
 	});
 
@@ -249,7 +252,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
 		.attr("width", width)
 		.attr("height", height)
 		.call(zoomHandler.z)
-		
+
 		//disable d3's zoom drag to override with my own
 		.on("mousedown.zoom", null)
 		.on("mousemove.zoom", null)
@@ -262,7 +265,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
 			zoomHandler.z.translate(zoomHandler.offset);
 
 			overLay.select(".popup").remove();
-			draw(zoomHandler.offset, zoomHandler.zoom);		
+			draw(zoomHandler.offset, zoomHandler.zoom);
 		}));
 
 	//create background and add axis to it
@@ -270,61 +273,61 @@ document.addEventListener("DOMContentLoaded", function(e) {
 	bg.append("g").attr("class", "axis").attr("id", "xaxis")
 		//create title for axis
 	bg.append("text").attr("id", "xAxisTitle").text("This Years EPS Growth (%)").style("font-size", "20px");
-	
+
 	bg.append("g").attr("class", "axis").attr("id", "yaxis");
 	bg.append("text").attr("id", "yAxisTitle").text("Next Years EPS Growth (%)")
 		.attr("transform", "rotate(-90)")
 		.style("font-size", "20px");
-	
-	
+
+
 	var overLay = svg.append("svg");
 	var circlePopup = overLay.append("g");
 
 	var Title = overLay.append("text").text("2-Year EPS Growth Overview")
 					.attr("x", function(){
-						return (window.innerWidth - this.getComputedTextLength())/2;	
+						return (window.innerWidth - this.getComputedTextLength())/2;
 					}).attr("y", "5%")
 					.style("font-size", "20")
 					.style("text-decoration", "underline");
 
-	
+
 
 	/*overLay.append("text").attr("id", "xAxisTitle").text("This Years EPS Growth (%)")
 			.attr("x", "20%").attr("y", "20%");
-	
+
 	overLay.append("text").attr("id", "yAxisTitle").text("Next Years EPS Growth (%)")
 			.attr("x", "40%").attr("y", "40%").attr("transform", "rotate(-90)");	*/
-	
-	
-	
+
+
+
 	function draw(translation, scale) {
 		chartScaler.scale(scale);
-		
+
 		//redraw axis
-		d3.select("#xaxis").attr("transform", "translate("+ 
+		d3.select("#xaxis").attr("transform", "translate("+
 				  translation[0] + ", " + parseFloat(chartScaler.yScale(0) + translation[1]) + ")" ).call(chartScaler.xAxis);
-		
-		d3.select("#yaxis").attr("transform", "translate(" +  
-				  parseFloat(chartScaler.xScale(0) + translation[0]) + "," +  translation[1]+")").call(chartScaler.yAxis);	
+
+		d3.select("#yaxis").attr("transform", "translate(" +
+				  parseFloat(chartScaler.xScale(0) + translation[0]) + "," +  translation[1]+")").call(chartScaler.yAxis);
 
 		//redraw axis title
-		d3.select("#xAxisTitle").attr("transform", "translate(" +  
+		d3.select("#xAxisTitle").attr("transform", "translate(" +
 				  parseFloat(chartScaler.xScale(chartScaler.xDomain[1] / 2) + translation[0]) + "," +
-				parseFloat(chartScaler.yScale(chartScaler.yDomain[1] / 8) +  translation[1])+")");	
-	
+				parseFloat(chartScaler.yScale(chartScaler.yDomain[1] / 8) +  translation[1])+")");
+
 		//redraw axis title
-		d3.select("#yAxisTitle").attr("transform", "translate(" +  
+		d3.select("#yAxisTitle").attr("transform", "translate(" +
 				parseFloat(chartScaler.xScale(chartScaler.xDomain[0] - 1) + translation[0]) + "," +
 				parseFloat(chartScaler.yScale(0) +  translation[1])+"), rotate(-90)");
-		
+
 		bg.selectAll(".scaledData")
 		.attr('x', function(d){
-				d.drawPosX = translation[0] + chartScaler.xScale(d.TY) 
-				- (scale *(d.EPS/epsScaleFactor/pointWidth/2));	
+				d.drawPosX = translation[0] + chartScaler.xScale(d.TY)
+				- (scale *(d.EPS/epsScaleFactor/pointWidth/2));
 
 				return d.drawPosX;
 			}).attr('y', function(d) {
-				 d.drawPosY = translation[1] + chartScaler.yScale(d.NY) 
+				 d.drawPosY = translation[1] + chartScaler.yScale(d.NY)
 				 - (scale *(d.EPS/epsScaleFactor/pointHeight/2));
 				return d.drawPosY;
 			})
@@ -332,17 +335,17 @@ document.addEventListener("DOMContentLoaded", function(e) {
 			.attr("height", function(d) { return scale * (d.EPS/epsScaleFactor/pointHeight)})
 			.on("click", function(d) {
 				(function(data) {
-						cloneSelection(circlePopup, data.this, "popup").on("mouseout", function(){ 
+						cloneSelection(circlePopup, data.this, "popup").on("mouseout", function(){
 							overLay.select(".popup").remove();
 						});
 
-				})(d);							
+				})(d);
 			}).each(function(d) {
 				var temp = scale * (d.EPS/epsScaleFactor/pointWidth);
 
 
-				if(temp < cullMin || temp > cullMax || d.drawPosX < -temp || d.drawPosY < -temp || 
-				   d.drawPosX > window.innerWidth || d.drawPosY > window.innerHeight) 
+				if(temp < cullMin || temp > cullMax || d.drawPosX < -temp || d.drawPosY < -temp ||
+				   d.drawPosX > window.innerWidth || d.drawPosY > window.innerHeight)
 					d.this.attr("display", "none");
 				else
 					d.this.attr("display", "block");
@@ -362,7 +365,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
 	});
 
 	zoomHandler.onZoom( function(e) {
-		draw(e.offset, e.zoom);	
+		draw(e.offset, e.zoom);
 	});
 
 
@@ -371,7 +374,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
 		//convert ( ) to negative numbers
 		if(val.match(/^\([\d,\.]*\)$/)) {
 			val = "-" + val.replace(/[\(\)]/g, '');
-		}	
+		}
 		return parseFloat(val);
 	}
 
@@ -397,9 +400,9 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
 						if(d.TY != "n/a" && d.NY != "n/a") {
 							d.TY = accountingValues(d.TY);
-							d.NY = accountingValues(d.NY);	
+							d.NY = accountingValues(d.NY);
 						}
-				});					
+				});
 
 
 				dataset.sort(function(a,b) { return b.EPS - a.EPS; });
@@ -407,20 +410,27 @@ document.addEventListener("DOMContentLoaded", function(e) {
 				chartScaler.xDomain = [d3.min(dataset, xData), d3.max(dataset, xData)];
 				chartScaler.yDomain = [d3.max(dataset, yData), d3.min(dataset, yData)];
 
+				//text-anchor is broken on everything but chrome on windows?
+				anchor = (function textAnchoring() {
+					if(navigator.userAgent.match(/chrome/i) && navigator.platform.match(/win/i)) {
+						return "middle";
+					}
+					return "inherit";
+				})();
 
 				//create a scalable container for display data
 				var idNumber = 0;
 				var dataCache = bg.selectAll("svg").data(dataset).enter().append("svg")
 								.attr("class", "scaledData")
 								.attr("id", function(d){
-									
+
 									var id = "plotPoint" + idNumber;
 									d.cssID = "#" + id;
 									idNumber+=1;
 									return id;
 								})
 								.each(function(d) {
-									d.this = bg.select(d.cssID);	
+									d.this = bg.select(d.cssID);
 								})
 								.attr("sector", function(d) { return d.Sector; })
 								.attr("width", pointWidth).attr("height", pointHeight)
@@ -432,7 +442,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
 								 var tmp = SectorColors.filter(function(a) {
 									if(a[d.Sector] != undefined) return a[d.Sector];
 								})[0][d.Sector];
-					
+
 								return tmp;
 							});
 
@@ -440,46 +450,46 @@ document.addEventListener("DOMContentLoaded", function(e) {
 				dataCache.append("text").attr("class", "plotDataName")
 						.text(function(d) { return d.Name; })
 						.attr("x", function() {
-							return (pointContentWidth - this.getComputedTextLength())/2;
-						}).attr("y", "22%");		
+							return (pointContentWidth - (this.getBBox().width || this.getComputedTextLength()))/2;
+						}).attr("y", "22%").attr("text-anchor", anchor);
 
 				dataCache.append("text").attr("class", "tickerLabel")
 							.text( function(d) { return d.Ticker; })
 							.attr("x", function(){
-								return (pointContentWidth - this.getComputedTextLength())/ 2;	
-							}).attr("y", "40%");
+								return (pointContentWidth - (this.getBBox().width || this.getComputedTextLength()))/ 2;
+							}).attr("y", "40%").attr("text-anchor", anchor);
 
 
 
 				dataCache.append("text").attr("class", "plotDataText")
 							.text(function(d) { return "EPS%: " + d.EPS; })
 							.attr("x", function() {
-								return (pointContentWidth - this.getComputedTextLength())/2;
-							}).attr("y", "55%");
+								return (pointContentWidth - (this.getBBox().width || this.getComputedTextLength()))/2;
+							}).attr("y", "55%").attr("text-anchor", anchor);
 
 				dataCache.append("text").attr("class", "plotDataText")
 							.text(function(d) { return "TY%: " + d.TY; })
 							.attr("x", function() {
-								return (pointContentWidth - this.getComputedTextLength())/2;
-							}).attr("y", "65%");
+								return (pointContentWidth - (this.getBBox().width || this.getComputedTextLength()))/2;
+							}).attr("y", "65%").attr("text-anchor", anchor);
 
 
 				dataCache.append("text").attr("class", "plotDataText")
 							.text(function(d) { return "NY%: " + d.NY; })
 							.attr("x", function() {
-								return (pointContentWidth - this.getComputedTextLength())/2;
-							}).attr("y", "75%");
+								return (pointContentWidth - (this.getBBox().width || this.getComputedTextLength()))/2;
+							}).attr("y", "75%").attr("text-anchor", anchor);
 
 				dataCache.append("text").attr("class", "plotDataText")
 							.text(function(d) { return "P/E%: " + d.PE; })
 							.attr("x", function() {
-								return (pointContentWidth - this.getComputedTextLength())/2;
-							}).attr("y", "85%");						
+								return (pointContentWidth - (this.getBBox().width || this.getComputedTextLength()))/2;
+							}).attr("y", "85%").attr("text-anchor", anchor);
 
 
 
-		
-			
+
+
 
 				draw(zoomHandler.offset, zoomHandler.zoom);
 		});
@@ -487,31 +497,31 @@ document.addEventListener("DOMContentLoaded", function(e) {
 	}
 
 	loadData('data1.csv');
-	
+
 	createLegend(SectorColors, function(d) {
 		var sectorFilter = d3.keys(d)[0];
-		
+
 		bg.selectAll("svg[sector=\"" + sectorFilter + "\"]")
 		.each(function(d) {
-				var obj = bg.select(d.cssID); 
+				var obj = bg.select(d.cssID);
 				if(obj.attr("visibility") == "hidden")
-					obj.attr("visibility", "visible");	
-				else 
-					obj.attr("visibility", "hidden");	
+					obj.attr("visibility", "visible");
+				else
+					obj.attr("visibility", "hidden");
 		});
-		
+
 	});
-	
-	
-	
+
+
+
 	d3.select(window).on("resize", function(){
 		chartScaler.xRange = [xPadding, window.innerWidth - xPadding];
 		chartScaler.yRange = [yPadding, window.innerHeight - yPadding];
 		d3.select("body svg").attr("width", window.innerWidth).attr("height", window.innerHeight);
-		draw(zoomHandler.offset, zoomHandler.zoom);	
-	});	
-	
-	
+		draw(zoomHandler.offset, zoomHandler.zoom);
+	});
+
+
 });
 
 
